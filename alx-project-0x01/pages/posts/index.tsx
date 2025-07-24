3e87a502ba74857867d455f545325afad059d802
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-07-25 00:12:23 +0100

    finshed 1

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "@/components/layout/Header";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default index;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "@/components/layout/Header";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      {" "}
+      <Header />
+    </div>
+  );
 };
 
 export default index;
